6c3a73e054114e4766b91960aebe501130bde13e
hanyunfan <frank.han@dell.com>
2024-11-24 23:02:18 -0600

    Update runme.sh

--- runme.sh
+++ runme.sh
@@ -8,7 +8,7 @@ HOSTS="localhost"
 MODELS="resnet50 cosmoflow"
 GPU_TYPE="h100"
 GPU_NUM=8 
-
+DATASET_PATH=/data
 
 #Auto-file parameters
 #CLIENT_NUM=1
@@ -32,20 +32,31 @@ TRAIN_FILE_NUM=$( ./benchmark.sh datasize --workload ${WORKLOAD} --accelerator-t
 sleep 3
  
 echo "Generating data files"
-if [[ -d /data/${WORKLOAD}-${GPU_TYPE}-${TRAIN_FILE_NUM} ]]; then
-    echo "Dataset folder /data/${WORKLOAD}-${GPU_TYPE}-${TRAIN_FILE_NUM} already exist, re-using it"
+if [[ -d /${DATASET_PATH}/${WORKLOAD}-${GPU_TYPE}-${TRAIN_FILE_NUM} ]]; then
+    echo "Dataset folder /${DATASET_PATH}/${WORKLOAD}-${GPU_TYPE}-${TRAIN_FILE_NUM} already exist, re-using it"
 else
-    echo "No exsiting dataset found, building it under /data/${WORKLOAD}-${GPU_TYPE}-${TRAIN_FILE_NUM}"
+    echo "No exsiting dataset found, building it under /${DATASET_PATH}/${WORKLOAD}-${GPU_TYPE}-${TRAIN_FILE_NUM}"
     sleep 3
-    ./benchmark.sh datagen --workload ${WORKLOAD} --accelerator-type ${GPU_TYPE} --num-parallel ${WRITER} --param dataset.num_files_train=${TRAIN_FILE_NUM} --param dataset.data_folder=/data/${WORKLOAD}-${GPU_TYPE}-${TRAIN_FILE_NUM}
+    ./benchmark.sh datagen --workload ${WORKLOAD} --accelerator-type ${GPU_TYPE} --num-parallel ${WRITER} --param dataset.num_files_train=${TRAIN_FILE_NUM} --param dataset.data_folder=/${DATASET_PATH}/${WORKLOAD}-${GPU_TYPE}-${TRAIN_FILE_NUM}
 fi
 
 echo "Running the test"
 sleep 3
-./benchmark.sh run -s ${HOSTS} --workload ${WORKLOAD} --accelerator-type ${GPU_TYPE} --num-accelerators ${GPU_NUM} --results-dir /results/${WORKLOAD}-${DATETIME} --param dataset.num_files_train=${TRAIN_FILE_NUM} --param dataset.data_folder=/data/${WORKLOAD}-${GPU_TYPE}-${TRAIN_FILE_NUM} -p reader.read_threads=${READER}
 
+for i in {1..5}; do
 
-AU=$( grep "train_au_meet_expectation"  /results/${WORKLOAD}-${DATETIME}/summary.json |awk '{print $2}'| cut -d\" -f2)
+    sync; echo 3 > /proc/sys/vm/drop_caches
+    sleep 30
+
+    ./benchmark.sh run -s ${HOSTS} --workload ${WORKLOAD} --accelerator-type ${GPU_TYPE} --num-accelerators ${GPU_NUM} --results-dir /results/${WORKLOAD}-${DATETIME}/run${i} --param dataset.num_files_train=${TRAIN_FILE_NUM} --param dataset.data_folder=/${DATASET_PATH}/${WORKLOAD}-${GPU_TYPE}-${TRAIN_FILE_NUM} -p reader.read_threads=${READER}
+
+    if [ $? -ne 0 ]; then
+        echo "Run $run_name failed. Check dlio.log for details."
+        exit 1
+    else
+    echo "Run $run_name completed successfully."
+
+AU=$( grep "train_au_meet_expectation"  /results/${WORKLOAD}-${DATETIME}/run${i}/summary.json |awk '{print $2}'| cut -d\" -f2)
 if [[  "$AU" != "success" ]]
 then
 	echo "AU fail, please check log"
@@ -53,8 +64,10 @@ else
 	
 	echo "Generating the report"
 	sleep 3
-	./benchmark.sh reportgen --results-dir /results/${WORKLOAD}-${DATETIME}
+	./benchmark.sh reportgen --results-dir /results/${WORKLOAD}-${DATETIME}/run${i} 
 fi
+done 
+
 
 done
 
